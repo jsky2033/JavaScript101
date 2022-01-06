1deeac5770414147d131fd3024602b070e932361
jsky2033 <jsky2033@gmail.com>
2022-01-05 23:00:54 -0500

    test2

---
+++ Project 3/app.js
@@ -0,0 +1,11 @@
+//elements
+const startGameBtn = document.getElementById("start-game-btn");
+
+
+//event listeners
+function startGame() {
+  console.log("Game is starting...");
+}
+
+//add event listeners
+startGameBtn.addEventListener("click", person.greet);
