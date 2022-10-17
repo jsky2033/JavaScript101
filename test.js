995d2ee05693beedaf41753ff7265c9acb878462
jsky2033 <jsky2033@gmail.com>
2022-10-16 20:33:52 -0400

    oop concepts

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
-const person = {
-  name: "Max",
-  greet() {
-    console.log(this.name);
-  },
-};
+function Person() {
+  console.log("hello");
+  // this = {};
+  this.age = 30;
+  this.name = "Max";
 
+  this.greet = function () {
+    console.log(`Hello I am ${this.name} and I am ${this.age} years old`);
+  };
+
+  // static functions, ca
+  Person.describe = function () {
+    console.log("Creating persons...");
+  };
+  // return this;
+}
+
+Person(); // doesn't have to be called with "new"
+
+const person = new Person(); // this 'new' keyword is what creates the instantation
 person.greet();
 
-console.log(Object.getOwnPropertyDescriptors(person));
